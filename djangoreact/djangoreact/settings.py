761fa6eaa8cc3266155e9320b66c580837401cad
Peter Hart <kozakhart@gmail.com>
2023-12-06 14:17:52 -0700

    settings

--- djangoreact/djangoreact/settings.py
+++ djangoreact/djangoreact/settings.py
@@ -184,6 +184,7 @@ CRISPY_TEMPLATE_PACK = "bootstrap5"
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = '/var/www/djangoreact/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
