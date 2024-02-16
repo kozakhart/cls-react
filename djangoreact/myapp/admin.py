c08f6dd6c5c6175a0c4463a4038167fd3f3c6c4c
Peter Hart <kozakhart@gmail.com>
2024-02-15 19:32:42 -0700

    admin

--- djangoreact/myapp/admin.py
+++ djangoreact/myapp/admin.py
@@ -1,11 +1,25 @@
 from django.contrib import admin
 from django.shortcuts import render
-from .models import OPIForm, Languages, Reasons, LanguageExperience, ComeToCampusReason
+from .models import OPIForm, Languages, Reasons, LanguageExperience, ComeToCampusReason, Courses
 from .forms import OPIForm_Forms
 import myapp.filemaker_api.filemaker_api as filemaker
 import myapp.box_api.box_api as box
 import pandas as pd
 
+
+class CoursesAdmin(admin.ModelAdmin):
+    search_fields = ['byu_course_key', 'language', 'language_abbreviation']
+    list_filter = ['type_language', 'type_civilization_culture', 'type_literature']
+    model = Courses
+
+    def save_model(self, request, obj, form, change):        
+        super().save_model(request, obj, form, change)
+
+        return obj.byu_course_key
+    def delete_model(self, request, obj):
+        obj.delete()
+
+admin.site.register(Courses, CoursesAdmin)
 
 class LanguagesAdmin(admin.ModelAdmin):
     model = Languages
