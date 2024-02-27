7fa837fa6c8bc13f32d50c221f5f0c40433f7293
Peter Hart <kozakhart@gmail.com>
2024-02-27 11:40:12 -0700

    test url

--- djangoreact/myapp/urls.py
+++ djangoreact/myapp/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path('email-confirmation/<str:encoded_string>/', views.email_confirmation, name='email_confirmation'),
     path('certificates', views.certificates, name='certificates'),
     path('certificates/review-certificate/record-id=<str:encoded_string>', views.review_certificate, name='review_certificate'),
+    path('test123456789', views.opi_form, name='test')
     #path('test', views.test, name='test'),
 
 ]
