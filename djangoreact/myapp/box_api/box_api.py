280ec9102f6adaafaea0ff4caa0b784982b5317e
Peter Hart <kozakhart@gmail.com>
2024-04-03 17:18:47 -0600

    box permissions fix

--- djangoreact/myapp/box_api/box_api.py
+++ djangoreact/myapp/box_api/box_api.py
@@ -1,207 +1,23 @@
-from datetime import datetime, timedelta
-import json
-import random
-import string
-from io import IOBase
-from typing import Optional, Union, Callable, TYPE_CHECKING, Any
-from cryptography.hazmat.backends import default_backend
-from cryptography.hazmat.primitives import serialization
-from cryptography.hazmat.primitives.asymmetric.rsa import RSAPrivateKey
-import jwt #PyJWT
-from boxsdk import Client
+from datetime import datetime
+from boxsdk import Client, CCGAuth
 from django.core.files.storage import FileSystemStorage
 import os
 from dotenv import load_dotenv
 
 load_dotenv()
 
-BOX_USER_ID = os.getenv('BOX_USER_ID')
-BOX_JWT = os.getenv('BOX_JWT')
 BOX_CLIENT_SECRET = os.getenv('BOX_CLIENT_SECRET')
 BOX_CLIENT_ID = os.getenv('BOX_CLIENT_ID')
 BOX_ENTERPRISE_ID = os.getenv('BOX_ENTERPRISE_ID')
-BOX_ACCESS_TOKEN = os.getenv('BOX_ACCESS_TOKEN')
-BOX_RSA_PRIVATE_PASSPHRASE = os.getenv('BOX_RSA_PRIVATE_PASSPHRASE')
-BOX_RSA_PRIVATE_KEY = os.getenv('BOX_RSA_PRIVATE_KEY')
 
-from boxsdk.auth.server_auth import ServerAuth
-
-if TYPE_CHECKING:
-    from boxsdk.network.network_interface import Network
-    from boxsdk.object.user import User
-
-
-class JWTAuth(ServerAuth):
-    """
-    Responsible for handling JWT Auth for Box Developer Edition. Can authenticate enterprise instances or app users.
-    """
-    _GRANT_TYPE = 'urn:ietf:params:oauth:grant-type:jwt-bearer'
-    test = 0
-    def __init__(
-            self,
-            rsa_private_key_file_sys_path: Optional[str] = None,
-            #rsa_private_key_passphrase: Optional[Union[str, bytes]] = None,
-            store_tokens: Optional[Callable[[str, str], None]] = None,
-            user: Optional[Union[str, 'User']] = BOX_USER_ID,
-            jwt_key_id: str = BOX_JWT,
-            client_secret: str = BOX_CLIENT_SECRET,
-            client_id: str = BOX_CLIENT_ID,
-            enterprise_id: Optional[str] = BOX_ENTERPRISE_ID,
-            box_device_id: str = '0',
-            box_device_name: str = '',
-            access_token: str = BOX_ACCESS_TOKEN,
-            session: Optional['Network'] = None,
-            jwt_algorithm: str = 'RS256',
-            rsa_private_key_passphrase: Optional[Union[str, bytes]] = BOX_RSA_PRIVATE_PASSPHRASE,
-            rsa_private_key_data: Union[bytes, IOBase, RSAPrivateKey] = BOX_RSA_PRIVATE_KEY,
-            **kwargs
-    ):
-        rsa_private_key = self._normalize_rsa_private_key(
-            file_sys_path=rsa_private_key_file_sys_path,
-            data=rsa_private_key_data,
-            passphrase=rsa_private_key_passphrase,
+def create_client():
+    auth = CCGAuth(
+        client_id=BOX_CLIENT_ID,
+        client_secret=BOX_CLIENT_SECRET,
+        enterprise_id=BOX_ENTERPRISE_ID
         )
-        del rsa_private_key_data
-        del rsa_private_key_file_sys_path
-        super().__init__(
-            client_id=client_id,
-            client_secret=client_secret,
-            enterprise_id=enterprise_id,
-            user=user,
-            store_tokens=store_tokens,
-            box_device_id=box_device_id,
-            box_device_name=box_device_name,
-            access_token=access_token,
-            refresh_token=None,
-            session=session,
-            **kwargs
-        )
-        self._rsa_private_key = rsa_private_key
-        self._jwt_algorithm = jwt_algorithm
-        self._jwt_key_id = jwt_key_id
-
-    def _fetch_access_token(self, subject_id: str, subject_type: str, now_time: Optional[datetime] = None) -> str:
-        system_random = random.SystemRandom()
-        jti_length = system_random.randint(16, 128)
-        ascii_alphabet = string.ascii_letters + string.digits
-        ascii_len = len(ascii_alphabet)
-        jti = ''.join(ascii_alphabet[int(system_random.random() * ascii_len)] for _ in range(jti_length))
-        if now_time is None:
-            now_time = datetime.utcnow()
-        now_plus_30 = now_time + timedelta(seconds=30)
-        assertion = jwt.encode(
-            {
-                'iss': self._client_id,
-                'sub': subject_id,
-                'box_sub_type': subject_type,
-                'aud': 'https://api.box.com/oauth2/token',
-                'jti': jti,
-                'exp': int((now_plus_30 - datetime(1970, 1, 1)).total_seconds()),
-            },
-            self._rsa_private_key,
-            algorithm=self._jwt_algorithm,
-            headers={
-                'kid': self._jwt_key_id,
-            },
-        )
-        data = {
-            'grant_type': self._GRANT_TYPE,
-            'client_id': self._client_id,
-            'client_secret': self._client_secret,
-            'assertion': assertion,
-        }
-        if self._box_device_id:
-            data['box_device_id'] = self._box_device_id
-        if self._box_device_name:
-            data['box_device_name'] = self._box_device_name
-        return self.send_token_request(data, access_token=None, expect_refresh_token=False)[0]
-
-    @classmethod
-    def _normalize_rsa_private_key(
-            cls,
-            file_sys_path: str,
-            data: Union[bytes, IOBase, RSAPrivateKey],
-            passphrase: Optional[Union[str, bytes]] = None
-    ) -> Any:
-        if len(list(filter(None, [file_sys_path, data]))) != 1:
-            raise TypeError("must pass exactly one of either rsa_private_key_file_sys_path or rsa_private_key_data")
-        if file_sys_path:
-            with open(file_sys_path, 'rb') as key_file:
-                data = key_file.read()
-        if hasattr(data, 'read') and callable(data.read):
-            data = data.read()
-        if isinstance(data, str):
-            try:
-                data = data.encode('ascii')
-            except UnicodeError as unicode_error:
-                raise TypeError(
-                    "rsa_private_key_data must contain binary data (bytes/str), not a text/unicode string"
-                ) from unicode_error
-
-        if isinstance(data, bytes):
-            passphrase = cls._normalize_rsa_private_key_passphrase(passphrase)
-            return serialization.load_pem_private_key(
-                data,
-                password=passphrase,
-                backend=default_backend(),
-            )
-        if isinstance(data, RSAPrivateKey):
-            return data
-        raise TypeError(
-            'rsa_private_key_data must be binary data (bytes/str), '
-            'a file-like object with a read() method, '
-            'or an instance of RSAPrivateKey, '
-            f'but got {data.__class__.__name__!r}'
-        )
-    @staticmethod
-    def _normalize_rsa_private_key_passphrase(passphrase: Any):
-        if isinstance(passphrase, str):
-            try:
-                return passphrase.encode('ascii')
-            except UnicodeError as unicode_error:
-                raise TypeError(
-                    "rsa_private_key_passphrase must contain binary data (bytes/str), not a text/unicode string"
-                ) from unicode_error
-
-        if not isinstance(passphrase, (bytes, type(None))):
-            raise TypeError(
-                f"rsa_private_key_passphrase must contain binary data (bytes/str), "
-                f"got {passphrase.__class__.__name__!r}"
-            )
-        return passphrase
-
-    @classmethod
-    def from_settings_dictionary(cls, settings_dictionary: dict, **kwargs: Any) -> 'JWTAuth':
-        """
-        Create an auth instance as defined by the given settings dictionary.
-        The dictionary should have the structure of the JSON file downloaded from the Box Developer Console.
-        :param settings_dictionary:       Dictionary containing settings for configuring app auth.
-        :return:                        Auth instance configured as specified by the config dictionary.
-        """
-        if 'boxAppSettings' not in settings_dictionary:
-            raise ValueError('boxAppSettings not present in configuration')
-        return cls(
-            client_id=settings_dictionary['boxAppSettings']['clientID'],
-            client_secret=settings_dictionary['boxAppSettings']['clientSecret'],
-            enterprise_id=settings_dictionary.get('enterpriseID', None),
-            jwt_key_id=settings_dictionary['boxAppSettings']['appAuth'].get('publicKeyID', None),
-            rsa_private_key_data=settings_dictionary['boxAppSettings']['appAuth'].get('privateKey', None),
-            rsa_private_key_passphrase=settings_dictionary['boxAppSettings']['appAuth'].get('passphrase', None),
-            **kwargs
-        )
-    @classmethod
-    def from_settings_file(cls, settings_file_sys_path: str, **kwargs: Any) -> 'JWTAuth':
-        """
-        Create an auth instance as defined by a JSON file downloaded from the Box Developer Console.
-        See https://developer.box.com/en/guides/authentication/jwt/ for more information.
-        :param settings_file_sys_path:    Path to the JSON file containing the configuration.
-        :return:                        Auth instance configured as specified by the JSON file.
-        """
-        with open(settings_file_sys_path, encoding='utf-8') as config_file:
-            config_dictionary = json.load(config_file)
-            return cls.from_settings_dictionary(config_dictionary, **kwargs)
-
-
+    client = Client(auth)
+    return client
 
 from requests.structures import CaseInsensitiveDict
 import requests
@@ -222,13 +38,6 @@ def get_folder(token, client):
 
 slat_folder = '185240535599'
 file_id = '1084898659538'
-
-def create_client():
-    config = JWTAuth()
-    token = config._fetch_access_token(BOX_USER_ID, 'user')
-    client = Client(config)
-    print('client created')
-    return client
 
 def create_folder(client, student_name, slat_folder):
     student_folder = client.folder(slat_folder).create_subfolder(student_name)
