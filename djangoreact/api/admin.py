6b94bf5125fd702cf03c426b7c3a4c6d90daaf3c
Peter Hart <kozakhart@gmail.com>
2024-07-11 14:57:18 -0600

    add model to admin

--- djangoreact/api/admin.py
+++ djangoreact/api/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import LASER_Queries
+from .models import LASER_Queries, OPIc_Diagnostic_Grid_Languages
 
 models = [LASER_Queries]
 for model in models:
